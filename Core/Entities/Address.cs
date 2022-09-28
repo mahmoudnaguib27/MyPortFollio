00ad9bb90fbe75f3c40a89c98e58f52f4a6c38ad
momo_ <momo_@DESKTOP-PFT0BRK>
2022-09-28 05:44:28 +0200

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class Address : EntityBase
+    {
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public int Number { get; set; }
+
+    }
+}
---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
+
