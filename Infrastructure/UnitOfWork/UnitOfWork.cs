4f69be8bb3aa5f0cc0dc69e7214eeb703fa61c09
momo_koka2010 <momo_koka2010@yahoo.com>
2022-10-03 00:53:00 +0200

    Modified In Infrastructure , Core And Web

---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        public IGenericRepository<T> Entity => throw new NotImplementedException();
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,6 +1,9 @@
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +15,19 @@ namespace Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("MyPortFolioDB"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +38,12 @@ namespace Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("defaultRoute","{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
