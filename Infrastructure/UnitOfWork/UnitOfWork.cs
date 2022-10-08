159907767f815e48704f68363f7853d471ea2589
momo_koka2010 <momo_koka2010@yahoo.com>
2022-10-08 17:26:41 +0200

    Modified

--- Infrastructure/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using Core.Interfaces;
+﻿ using Core.Interfaces;
+using Infrastructure.Repository;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,11 +8,23 @@ namespace Infrastructure.UnitOfWork
 {
     public class UnitOfWork<T> : IUnitOfWork<T> where T : class
     {
-        public IGenericRepository<T> Entity => throw new NotImplementedException();
+        private readonly DataContext _context;
+        private IGenericRepository<T> _entity;
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Entity
+        {
+            get
+            {
+                return _entity ?? (_entity = new GenericRepository<T>(_context));
+            }
+        }
 
         public void Save()
         {
-            throw new NotImplementedException();
+            _context.SaveChanges();
         }
     }
 }
